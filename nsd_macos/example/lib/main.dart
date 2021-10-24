dfb98e664708751c65fded9331a93ec3a71dff20
Sebastian Haberey <sebastian@haberey.com>
2021-10-24 12:50:06 +0200

    macos: cleanups (example app)

--- nsd_macos/example/lib/main.dart
+++ nsd_macos/example/lib/main.dart
@@ -1,4 +1,3 @@
 void main() {
-  // The example app is in the main module.
-  // This example app is only a stub needed for the Xcode projects.
+  // This example app is only a stub needed for the Xcode project.
 }
