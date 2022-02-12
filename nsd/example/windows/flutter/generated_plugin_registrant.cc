0103ceb31f505965c109697210abec9fa6e9cef0
Sebastian Haberey <sebastian@haberey.com>
2022-02-12 13:54:09 +0700

    added windows runner

---
+++ nsd/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <nsd_windows/nsd_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  NsdWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("NsdWindowsPlugin"));
+}
