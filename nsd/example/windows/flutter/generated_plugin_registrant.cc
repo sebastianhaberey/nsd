0376f47de0b5ee48b6c8d7b12be45dccfcb8281a
Sebastian Haberey <sebastian@haberey.com>
2022-05-23 16:06:33 +0200

    switched main project back to nsd_windows

--- nsd/example/windows/flutter/generated_plugin_registrant.cc
+++ nsd/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <nsd_windows_v2/nsd_windows_v2_plugin_c_api.h>
+#include <nsd_windows/nsd_windows_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  NsdWindowsV2PluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("NsdWindowsV2PluginCApi"));
+  NsdWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("NsdWindowsPluginCApi"));
 }
--- nsd/example/windows/flutter/generated_plugins.cmake
+++ nsd/example/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  nsd_windows_v2
+  nsd_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
